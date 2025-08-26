1460c0d5992db34141bb0d0fe0c51782567036f8
jose <jruizzapata38@gmail.com>
2025-08-25 22:53:34 -0500

    Mejorar validación en la función calculateTip

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
   function calculateTip(billAmount, tipPercentage) {
-
+    return billAmount * (tipPercentage / 100);
   }
 console.log(calculateTip(100, 10)); // Output: 10
 console.log(calculateTip(1524.33, 25)); // Output: 381.0825
